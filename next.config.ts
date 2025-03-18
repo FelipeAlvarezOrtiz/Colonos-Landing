9dc28c887608126d48d353e4d46497c910f51e98
fsegovia <felipe.segovia.rod@gmail.com>
2025-03-18 13:10:22 -0300

    Se elimina la optimización de imagenes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true, // Esto es para que Next.js no optimice las imágenes
+  },
 };
 
 export default nextConfig;
